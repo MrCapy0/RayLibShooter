9885fb2741756b6408e5f2a12c2cef25d69bc434
Ruan <hellohumans.indie@gmail.com>
2026-03-01 09:21:56 -0400

    Revert "add project"
    This reverts commit c5f08a18f55f49bb55b62db674dbade33499e99c.

--- build/obj/Debug/main.d
+++
@@ -1,6 +0,0 @@
-obj/Debug/main.o: ../src/main.cpp /usr/include/stdc-predef.h \
- ../libs/raylib/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
-/usr/include/stdc-predef.h:
-../libs/raylib/include/raylib.h:
-/usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h:
